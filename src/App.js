88bbb4ce82a17d006bb103c8e668eec2e4b8aa20
Chris Post <chris.post@sigfig.com>
2021-12-23 16:57:35 -0700

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
-  return (
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ isPassedToWithAuthenticator, signOut, user }) {  
+
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
+return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <button onClick={signOut}>Sign out</button> 
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
